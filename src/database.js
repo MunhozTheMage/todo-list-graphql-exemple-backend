ef16eea3af74f5cad100ed14839475997103219a
Gabriel Munhoz Nóbrega <gmnobrega0704@gmail.com>
2022-05-11 21:54:30 -0300

    Uses sqlite as database

--- src/database.js
+++ src/database.js
@@ -1,12 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const db = new Sequelize({
-  dialect: "mysql",
-  host: "localhost",
-  port: "3307",
-  username: "root",
-  password: "root",
-  database: "todolist",
+  dialect: "sqlite",
+  storage: "db/database.sqlite",
 });
 
 module.exports = db;
